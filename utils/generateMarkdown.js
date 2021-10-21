ed43d1b8afca9a991f69405d7b9156d645bd2f9d
kperkins97 <perkins.kelsey@yahoo.com>
2021-10-21 17:06:09 -0500

    testing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,32 +1,29 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# Your ReadMe:
+  return `
 # Project Title: 
   ${data.title} 
+
+## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Preview](#preview)
+  - [Credits](#contributing)
+  - [License](#license)
+  - [Questions](#questions)
+
 # Description: 
   ${data.description}
-# Installation:
+## Installation:
   ${data.installation} 
-# Usage:
+## Usage:
   ${data.usage} 
-# Contributing:
+## Contributing:
   ${data.contributors} 
 # License:
   ${data.license} 
 # Questions:
-Click here for my Github:(https://github.com/${data.github}) 
+Click here for my Github: https://github.com/${data.github}
 # Email Me:
   Shoot me an email!: ${data.email} 
 `;
@@ -34,3 +31,15 @@ Click here for my Github:(https://github.com/${data.github})
 module.exports = generateMarkdown;
 
 
+
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// function renderLicenseBadge(license) {}
+
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {}
+
+// // TODO: Create a function that returns the license section of README
+// // If there is no license, return an empty string
+// function renderLicenseSection(license) {}
